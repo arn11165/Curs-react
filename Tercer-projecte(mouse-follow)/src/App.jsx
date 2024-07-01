75ff4c0fff6bf7cb2630df319ba5852970224c06
agispertt <arnau.gispert.coll@gmail.com>
2024-07-01 12:25:41 +0200

    Mouse Follower Finalitzat

--- Tercer-projecte(mouse-follow)/src/App.jsx
+++ Tercer-projecte(mouse-follow)/src/App.jsx
@@ -1,48 +1,66 @@
 import { useEffect, useState } from "react"
 
-
-function App() {
+const FollowMouse = () => {
 
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({x: 0, y: 0})  
+  console.log(enabled)
 
   useEffect(() => {
-    console.log("el valor de enabled ha cambiado a", enabled)
 
     const handleMove = (event) => {
       const {clientX, clientY} = event
-      console.log('handleMove', {clientX, clientY})
       setPosition({x: clientX, y: clientY})
     }
   
     if(enabled) {
       window.addEventListener('pointermove', handleMove)
     }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled])
+
+  useEffect(() => {
+    document.body.style.cursor = enabled ? 'none' : 'default'
+    
+  })
 
   return (
     <>
-    <main>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: '#09f',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20, 
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+    }}>
 
-      <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20, 
-        width: 40,
-        height: 40,
-        transform: `translate(${position.x}px, ${position.y}px)`,
-      }}>
+    </div>
+    <button onClick={() => setEnabled(!enabled)}>
+    {enabled ? 'Desactivar' : 'Activar'} seguir punter
+    </button>
+    </>
+  )
+}
 
-      </div>
+function App() {
 
-      <button onClick={() => setEnabled(!enabled)}>
-          {enabled ? 'Desactivar' : 'Activar'} seguir punter
-      </button>
-    </main>
+  const [mounted, setMounted] = useState(true)
+
+  return (
+    <>
+      <main>
+        {mounted && <FollowMouse />}
+        
+      </main>
+      
     </>
      
   )
